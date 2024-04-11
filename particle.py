6d4bfc06496d11fc44b90e1e1d2109113d407e07
Tobias Samsonsen <Tobias.samsonsen@gmail.com>
2024-04-11 10:19:08 +0200

    particle drawing

--- particle.py
+++ particle.py
@@ -1,6 +1,11 @@
+import pygame
+
 class Particle:
     def __init__(self, x, y, color, velocity):
         self.x = x
         self.y = y
         self.color = color
         self.velocity = velocity
+    
+    def draw(self, window, size):
+        pygame.draw.circle(window, self.color, (self.x, self.y), size)
