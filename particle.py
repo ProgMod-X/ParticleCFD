005d8bd30da1e2ef1d89973087a25fa9d0f039bd
RohaanMooken <rohaanmooken@gmail.com>
2024-04-12 08:53:18 +0200

    Pygame clamp does not work

--- particle.py
+++ particle.py
@@ -15,8 +15,18 @@ class Particle:
 
         win_width, win_height = window.get_size()
 
+        # The below is the first iteration of the bounding box
+        # if self.x < 0:
+        #     self.x = 0 + self.size
+        # elif self.x > win_width:
+        #     self.x = win_width - self.size
+        # if self.y < 0:
+        #     self.y = 0 + self.size
+        # elif self.y > win_height:
+        #     self.y = win_height - self.size
+
         # Make sure the particles are inside the bounding box, which is set to the window size.
-        pygame.math.clamp(self.x, self.size, win_width - self.size)
-        pygame.math.clamp(self.y, self.size, win_height - self.size)
+        self.x = max(self.size, min(self.x, win_width - self.size))
+        self.y = max(self.size, min(self.y, win_height - self.size))
 
         pygame.draw.circle(window, self.color, (self.x, self.y), self.size)
