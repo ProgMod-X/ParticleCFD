1c16b501efb96b1a9f15a6d3665edecc30c572bf
RohaanMooken <rohaanmooken@gmail.com>
2024-04-22 08:36:11 +0200

    Remove dupe sim func

--- main.py
+++ main.py
@@ -41,29 +41,6 @@ def deltaTime() -> float:
     
     return delta_time
 
-def simulate(dt):
-    global particle_list
-    global last_particle_list
-
-    last_particle_list = [p for p in particle_list]
-    particle_list = []
-    WIN.fill((0, 0, 0))
-    for cur_particle in last_particle_list:
-        # Calculate the new position and velocity based on the forces
-        new_velocity = cur_particle.velocity + (force(IRL_GRAVITY, cur_particle)) * dt
-        new_position = cur_particle.position + (cur_particle.velocity) * dt
-        
-        # Create a new Particle instance with the updated properties
-        new_particle = particle.Particle(new_position, new_velocity, GREEN, PARTICLE_PIXEL_RADIUS, DAMPENING_EFFECT)
-        
-        # Add the updated particle to the new particle_list
-        particle_list.append(new_particle)
-        
-        # Draw the updated particle
-        new_particle.draw(WIN)
-    
-    pygame.display.flip()
-
 def force(gravity: pygame.Vector2, particle: particle.Particle) -> pygame.Vector2:
     force = gravity
     force += repulsion(particle)
