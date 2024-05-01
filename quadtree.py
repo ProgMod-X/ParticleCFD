0b1d45407566b8e91aee99e3cef79c7069b58bf6
RohaanMooken <rohaanmooken@gmail.com>
2024-05-01 20:16:26 +0200

    Create a part of the quadtree datastructure

---
+++ quadtree.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Rectangle:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w
+        self.h = h
+
+    def contains(self, point):
+        return (
+            point.x >= self.x - self.w
+            and point.x < self.x + self.w
+            and point.y >= self.y - self.h
+            and point.y < self.y + self.h
+        )
+
+
+class QuadTree:
+    def __init__(self, boundary, n):
+        self.boundary = boundary
+        self.capacity = n
+        self.points = []
+        self.divided = False
+    
+    def subdivide(self):
+        x = self.boundary.x
+        y = self.boundary.y
+        w = self.boundary.w
+        h = self.boundary.h
+
+        ne = Rectangle(x + w / 2, y - h / 2, w / 2, h / 2)
+        self.northeast = QuadTree(ne, self.capacity)
+
+        nw = Rectangle(x - w / 2, y - h / 2, w / 2, h / 2)
+        self.northwest = QuadTree(nw, self.capacity)
+        
+        se = Rectangle(x + w / 2, y + h / 2, w / 2, h / 2)
+        self.southeast = QuadTree(se, self.capacity)
+        
+        sw = Rectangle(x - w / 2, y + h / 2, w / 2, h / 2)
+        self.southwest = QuadTree(sw, self.capacity)
+        
+        self.divided = True
+    
+    def insert(self, point):
+        if not self.boundary.contains(point):
+            return False
+
+        if len(self.points) < self.capacity:
+            self.points.append(point)
+            return True
+        else:
+            if not self.divided:
+                self.subdivide()
+            if self.northeast.insert(point):
+                return True
+            elif self.northwest.insert(point):
+                return True
+            elif self.southeast.insert(point):
+                return True
+            elif self.southwest.insert(point):
+                return True
